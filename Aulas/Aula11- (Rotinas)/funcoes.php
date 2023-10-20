1a88261e81dd25e7be331d30b2fdc66a3c3c52a2
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-10-20 15:22:07 -0300

    Funções index

---
+++ Aulas/Aula11- (Rotinas)/funcoes.php
@@ -0,0 +1,10 @@
+<?php
+    function ola(){
+        echo "<h2>Olá, Mundo!</h1>";
+    }
+
+    function mostrarValor($valor){
+        echo "<h2>Acabei de receber o valor $valor</h2>";
+    }
+
+?>
