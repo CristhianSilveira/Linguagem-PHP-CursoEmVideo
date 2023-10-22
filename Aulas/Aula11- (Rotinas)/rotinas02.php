5bac2c06ba70b6579f504ab536bc3e40ae038a18
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-10-22 11:36:20 -0300

    Rotinas02 Index

---
+++ Aulas/Aula11- (Rotinas)/rotinas02.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rotinas PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body> 
+
+<div>
+    <?php 
+    include "funcoes.php";
+    echo "<h1>Testando novas funções</h1>";
+    ola();
+    mostrarValor(4);
+    echo "<h2>Finalizando Programa...</h2>";
+
+    ?>
+
+    <a href="rotinas01.php">Voltar</a>
+</div>
+    
+</body>
+</html>
